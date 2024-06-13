c84704d269dc7e9937fd6b90e96b1e2a961c967d
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2024-06-13 16:39:10 +0200

    adjusted main view and added the logo provisionally

--- Frontend/taskmanager/lib/view/main_view.dart
+++ Frontend/taskmanager/lib/view/main_view.dart
@@ -15,56 +15,90 @@ class _MainViewState extends State<MainView> {
 
   @override
   Widget build(BuildContext context) {
+    Color myGreen = Color(0xFF3E9671);
+
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey[900], // Canvia el color de fons a gris fosc
         appBar: AppBar(
-          title: const Text('Prova'),
-        ),
-        body: Row(
-          children: [
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/robot_page');
-                        },
-                        child: const Text('Gestiona els robots'),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height / 4,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+          backgroundColor: Colors.grey[900], // Canvia el color de la barra d'aplicacions a gris fosc
+          title: const Text('Agro Task Manager', style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.power_settings_new, color: Colors.red),
+              onPressed: () {
+                // Defineix l'acció del botó d'apagar aquí
+              },
             ),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/crop_page');
-                        },
-                        child: const Text('Gestiona els conreus'),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height / 4,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            )
           ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: myGreen,
+                  child: Center(
+                      child: Image.asset('resources/logo.png')),
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                width: 200,
+                height: 100,
+                color: myGreen,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black, backgroundColor: myGreen, // Canvia el color del text
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/robot_page');
+                  },
+                  child: const Text("Robots' tasks"),
+                ),
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 100,
+                    color: myGreen,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black, backgroundColor: myGreen, // Canvia el color del text
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/crop_page');
+                      },
+                      child: const Text('Crops & Calendar'),
+                    ),
+                  ),
+                  SizedBox(width: 50),
+                  Container(
+                    width: 200,
+                    height: 100,
+                    color: myGreen,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black, backgroundColor: myGreen, // Canvia el color del text
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/settings_page');
+                      },
+                      child: const Text('Settings'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
