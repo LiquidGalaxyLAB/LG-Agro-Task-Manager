95f94fe0f252e9e78aef8cb6048a7c2c27ff45fb
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2024-07-08 17:04:14 +0200

    organitzat a main

--- lib/view/main_view.dart
+++ lib/view/main_view.dart
@@ -42,156 +42,18 @@ class _MainViewState extends State<MainView> {
                 children: [
                   SizedBox(height: screenHeight * 0.05),
                   Center(
-                    child: Image.asset('resources/images/logo.png', width: logoSize, height: buttonSize),
+                    child: Image.asset(
+                      'resources/images/logo.png',
+                      width: logoSize,
+                      height: buttonSize,
+                    ),
                   ),
                   SizedBox(height: screenHeight * 0.03),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/crop_page');
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              width: screenWidth / 3,
-                              height: screenHeight / 4,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('resources/images/crops.jpg'),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: screenWidth / 3,
-                              height: screenHeight / 4,
-                              color: Colors.black.withOpacity(0.3),
-                              child: Center(
-                                child: Text(
-                                  'Crops',
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/robot_page');
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              width: screenWidth / 3,
-                              height: screenHeight / 4,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('resources/images/robots.png'),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: screenWidth / 3,
-                              height: screenHeight / 4,
-                              color: Colors.black.withOpacity(0.3),
-                              child: Center(
-                                child: Text(
-                                  'Robots',
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/settings_page');
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              width: screenWidth / 3,
-                              height: screenHeight / 4,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('resources/images/settings.jpg'),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: screenWidth / 3,
-                              height: screenHeight / 4,
-                              color: Colors.black.withOpacity(0.3),
-                              child: Center(
-                                child: Text(
-                                  'Settings',
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.8),
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                  _buildCategoryRow(screenWidth, screenHeight, context),
                   SizedBox(height: screenHeight * 0.05),
-                  Text(
-                    'Plants in season:',
-                    style: TextStyle(color: myGreen, fontSize: 16),
-                  ),
+                  _buildSectionHeader('Plants in season:', myGreen),
                   const SizedBox(height: 10),
-                  Container(
-                    width: screenWidth * 0.8,
-                    height: screenHeight * 0.3,
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[800],
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(color: Colors.white),
-                    ),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: viewModel.cropsInCurrentFortnight.length,
-                      itemBuilder: (context, index) {
-                        final crop = viewModel.cropsInCurrentFortnight[index];
-                        return Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(color: Colors.white, width: 0.5),
-                            ),
-                          ),
-                          child: ListTile(
-                            title: Text(
-                              crop.cropName,
-                              style: TextStyle(color: myGreen),
-                            ),
-                            subtitle: Text(
-                              viewModel.getTaskText(crop),
-                              style: TextStyle(color: myGreen),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                  _buildPlantsInSeasonList(screenWidth, screenHeight, myGreen),
                 ],
               ),
             ),
@@ -207,6 +69,135 @@ class _MainViewState extends State<MainView> {
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryRow(double screenWidth, double screenHeight, BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildCategoryItem(
+          context,
+          '/crop_page',
+          'Crops',
+          'resources/images/crops.jpg',
+          screenWidth,
+          screenHeight,
+        ),
+        _buildCategoryItem(
+          context,
+          '/robot_page',
+          'Robots',
+          'resources/images/robots.png',
+          screenWidth,
+          screenHeight,
+        ),
+        _buildCategoryItem(
+          context,
+          '/settings_page',
+          'Settings',
+          'resources/images/settings.jpg',
+          screenWidth,
+          screenHeight,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCategoryItem(
+      BuildContext context,
+      String route,
+      String title,
+      String imagePath,
+      double screenWidth,
+      double screenHeight,
+      ) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, route);
+      },
+      child: Stack(
+        children: [
+          Container(
+            width: screenWidth / 3,
+            height: screenHeight / 4,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+          Container(
+            width: screenWidth / 3,
+            height: screenHeight / 4,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSectionHeader(String title, Color myGreen) {
+    return Text(
+      title,
+      style: TextStyle(
+        color: myGreen,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildPlantsInSeasonList(double screenWidth, double screenHeight, Color myGreen) {
+    return Container(
+      width: screenWidth * 0.8,
+      height: screenHeight * 0.3,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[850],
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(color: myGreen),
+      ),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: viewModel.cropsInCurrentFortnight.length,
+        itemBuilder: (context, index) {
+          final crop = viewModel.cropsInCurrentFortnight[index];
+          return Container(
+            margin: const EdgeInsets.symmetric(vertical: 4.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[800],
+              borderRadius: BorderRadius.circular(5.0),
+              border: Border.all(color: Colors.white.withOpacity(0.5)),
+            ),
+            child: ListTile(
+              title: Text(
+                crop.cropName,
+                style: TextStyle(color: myGreen),
+              ),
+              subtitle: Text(
+                viewModel.getTaskText(crop),
+                style: const TextStyle(color: Colors.white70),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
