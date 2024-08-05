2554680413c34fc85933d947d55b2789b89799e4
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2024-08-05 16:23:32 +0200

    Update TASK

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -1,14 +1,20 @@
 class Task {
   String taskName;
   double completionPercentage;
+  Function(double) callback = (double _) {};
 
   Task(this.taskName, this.completionPercentage);
+
+  void subscribeChanges(Function(double) callback) {
+    this.callback = callback;
+  }
 
   Future<void> simulateCompletion() async {
     completionPercentage = 0;
     while (completionPercentage < 100) {
       await Future.delayed(const Duration(seconds: 2));
       completionPercentage += 2;
+      callback(completionPercentage);
     }
   }
 }
