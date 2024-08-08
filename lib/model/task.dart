3e512ab8b67d85d826f625fccfbc9568b205999e
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2024-08-08 16:55:26 +0200

    willPopScope added

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -19,4 +19,8 @@ class Task {
       }
     }
   }
+
+  void removeCallback() {
+    callback = null;
+  }
 }
--- lib/view/robots_view.dart
+++ lib/view/robots_view.dart
@@ -25,167 +25,186 @@ class _RobotsViewState extends State<RobotsView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: customDarkGrey,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
+    return WillPopScope(
+      onWillPop: () async {
+        viewModel.getCurrentRobot().currentTask?.removeCallback();
+        viewModel.setCurrentRobot(Robot.empty());
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: customDarkGrey,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () async {
+              viewModel.setCurrentRobot(Robot.empty());
+              Navigator.of(context).pop();
+            },
+          ),
+          title: const Text('Robots view'),
+          backgroundColor: customGreen,
         ),
-        title: const Text('Robots view'),
-        backgroundColor: customGreen,
-      ),
-      body: Consumer<RobotsViewModel>(
-        builder: (context, viewModel, child) {
-          return Column(
-            children: [
-              const SizedBox(height: 20),
-              Text(
-                viewModel.getCurrentRobot().name,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: customGreen,
+        body: Consumer<RobotsViewModel>(
+          builder: (context, viewModel, child) {
+            return Column(
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  viewModel.getCurrentRobot().name,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: customGreen,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Current Task: ${viewModel.getCurrentRobot().currentTask != null ? viewModel.getCurrentRobot().currentTask?.taskName : ""}',
-                      style: const TextStyle(color: customGreen, fontSize: 16),
-                    ),
-                    const SizedBox(height: 10),
-                    viewModel.getCurrentRobot().currentTask != null
-                        ? Column(
-                            children: [
-                              CircularPercentIndicator(
-                                radius: 50.0,
-                                lineWidth: 10.0,
-                                percent: percentage / 100,
-                                center: Text(
-                                  '${percentage.toStringAsFixed(1)}%',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Current Task: ${viewModel.getCurrentRobot().currentTask != null ? viewModel.getCurrentRobot().currentTask?.taskName : ""}',
+                        style:
+                            const TextStyle(color: customGreen, fontSize: 16),
+                      ),
+                      const SizedBox(height: 10),
+                      viewModel.getCurrentRobot().currentTask != null
+                          ? Column(
+                              children: [
+                                CircularPercentIndicator(
+                                  radius: 50.0,
+                                  lineWidth: 10.0,
+                                  percent: percentage / 100,
+                                  center: Text(
+                                    '${percentage.toStringAsFixed(1)}%',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  progressColor: customGreen,
+                                  backgroundColor: customLightGrey,
+                                ),
+                                const SizedBox(height: 10),
+                              ],
+                            )
+                          : const Text(
+                              'No current task',
+                              style:
+                                  TextStyle(color: customGreen, fontSize: 16),
+                            ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height / 4,
+                          width: MediaQuery.of(context).size.width,
+                          child: ListView.builder(
+                            itemCount: viewModel
+                                .getCurrentRobot()
+                                .remainingTasks
+                                .length,
+                            itemBuilder: (context, index) {
+                              return Card(
+                                color: customLightGrey,
+                                child: ListTile(
+                                  title: Text(
+                                    viewModel
+                                        .getCurrentRobot()
+                                        .remainingTasks[index]
+                                        .taskName,
+                                    style: const TextStyle(
+                                        color: Colors.white, fontSize: 16),
                                   ),
                                 ),
-                                progressColor: customGreen,
-                                backgroundColor: customLightGrey,
-                              ),
-                              const SizedBox(height: 10),
-                            ],
-                          )
-                        : const Text(
-                            'No current task',
-                            style: TextStyle(color: customGreen, fontSize: 16),
-                          ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                        height: MediaQuery.of(context).size.height / 4,
-                        width: MediaQuery.of(context).size.width,
-                        child: ListView.builder(
-                          itemCount:
-                              viewModel.getCurrentRobot().remainingTasks.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              color: customLightGrey,
-                              child: ListTile(
-                                title: Text(
-                                  viewModel
-                                      .getCurrentRobot()
-                                      .remainingTasks[index]
-                                      .taskName,
-                                  style: const TextStyle(
-                                      color: Colors.white, fontSize: 16),
-                                ),
-                              ),
+                              );
+                            },
+                          )),
+                      ElevatedButton(
+                        onPressed: () async {
+                          if (context.mounted) {
+                            await Navigator.pushNamed(
+                              context,
+                              '/add_task',
+                              arguments: {
+                                'tmTemp': viewModel.getTaskManager(),
+                                'robot': viewModel.getCurrentRobot(),
+                              },
                             );
-                          },
-                        )),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (context.mounted) {
-                          await Navigator.pushNamed(
+                          }
+                          viewModel.update();
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: customGreen),
+                        child: const Text(
+                          'Add tasks',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          viewModel.visualizeTask();
+                          Navigator.pushNamed(
                             context,
-                            '/add_task',
+                            '/maps_view',
                             arguments: {
-                              'tmTemp': viewModel.getTaskManager(),
-                              'robot': viewModel.getCurrentRobot(),
+                              'field': viewModel.getCurrentRobot().field,
                             },
                           );
-                        }
-                        viewModel.update();
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: customGreen),
-                      child: const Text(
-                        'Add tasks',
-                        style: TextStyle(color: Colors.black, fontSize: 16),
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: customGreen),
+                        child: const Text(
+                          'Visualize',
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        viewModel.visualizeTask();
-                        Navigator.pushNamed(
-                          context,
-                          '/maps_view',
-                          arguments: {
-                            'field': viewModel.getCurrentRobot().field,
-                          },
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: customGreen),
-                      child: const Text(
-                        'Visualize',
-                        style: TextStyle(color: Colors.black, fontSize: 16),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const Spacer(),
-              SizedBox(
-                  height: MediaQuery.of(context).size.height / 10,
-                  child: RobotPage(
-                    robots: viewModel.getRobots(),
-                    onRobotSelected: (robot) {
-                      setState(() {
-                        viewModel.fetchTaskManager();
-                        viewModel.setCurrentRobot(robot);
-                        robot.currentTask?.callback = updateValues;
-                      });
-                    },
-                    onRobotDeleted: (Robot r) async {
-                      await viewModel.deleteRobot();
-                      setState(() {});
-                    },
-                  )),
-            ],
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-            return AddRobotWidget(
-              onSubmit: (robotName, robotIP, robotSN, field) async {
-                final viewModel =
-                    Provider.of<RobotsViewModel>(context, listen: false);
-                await viewModel.createRobot(robotName, robotSN, robotIP, field);
-                if (context.mounted) Navigator.of(context).pop();
-              },
+                const Spacer(),
+                SizedBox(
+                    height: MediaQuery.of(context).size.height / 10,
+                    child: RobotPage(
+                      robots: viewModel.getRobots(),
+                      onRobotSelected: (robot) {
+                        setState(() {
+                          viewModel
+                              .getCurrentRobot()
+                              .currentTask
+                              ?.removeCallback();
+                          viewModel.fetchTaskManager();
+                          viewModel.setCurrentRobot(robot);
+                          robot.currentTask?.callback = updateValues;
+                        });
+                      },
+                      onRobotDeleted: (Robot r) async {
+                        await viewModel.deleteRobot();
+                        setState(() {});
+                      },
+                    )),
+              ],
             );
-          }));
-        },
-        backgroundColor: customGreen,
-        child: const Icon(Icons.add),
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+              return AddRobotWidget(
+                onSubmit: (robotName, robotIP, robotSN, field) async {
+                  final viewModel =
+                      Provider.of<RobotsViewModel>(context, listen: false);
+                  await viewModel.createRobot(
+                      robotName, robotSN, robotIP, field);
+                  if (context.mounted) Navigator.of(context).pop();
+                },
+              );
+            }));
+          },
+          backgroundColor: customGreen,
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
